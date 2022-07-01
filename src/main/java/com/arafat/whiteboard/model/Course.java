71c97fe2edb65110c6d1775f384aa43751f77a32
Araf020 <Araf020@users.noreply.github.com>
2022-07-01 15:13:29 +0600

    Course controller done

--- src/main/java/com/arafat/whiteboard/model/Course.java
+++ src/main/java/com/arafat/whiteboard/model/Course.java
@@ -6,23 +6,42 @@ import javax.persistence.*;
 @Table(name = "course")
 public class Course {
 
+
+    //set auto-increment to true
+    //set a initial value of 101
+    //set allocation size to 1
+    //set increment by 1
+    //set initial value of 101
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @SequenceGenerator(name = "course_id_seq", sequenceName = "course_id_seq", allocationSize = 1, initialValue = 101)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "course_id_seq")
+
     private long course_id;
 
-    @Column(name = "session_id",unique = true)
-    private String sessionId;
+    @Column(name = "course_code",unique = true)
+    private String courseCode;
 
-    @Column(name = "course_title")
+    @Column(name = "course_title", unique = true)
     private String courseTitle;
 
     @Column(name = "credit_hour")
     private double creditHour;
 
+    @Column (name = "course_description")
+    private String courseDescription;
+
+    @Column (name = "course_marks")
+    private Double courseMarks;
+
+    @Column (name = "course_grade")
+    private String courseGrade;
+    @Column (name = "course_prerequisite")
+    private String coursePrerequisite;
+
     public Course(){}
 
-    public Course(String sessionId, String courseTitle, double creditHour) {
-        this.sessionId = sessionId;
+    public Course(String course_code, String courseTitle, double creditHour) {
+        this.courseCode = course_code;
         this.courseTitle = courseTitle;
         this.creditHour = creditHour;
     }
@@ -33,12 +52,12 @@ public class Course {
 
 
 
-    public String getSessionId() {
-        return sessionId;
+    public String getCourseId() {
+        return courseCode;
     }
 
-    public Course setSessionId(String sessionId) {
-        this.sessionId = sessionId;
+    public Course setCourseId(String courseCode) {
+        this.courseCode = Course.this.courseCode;
         return this;
     }
 
@@ -57,6 +76,44 @@ public class Course {
 
     public Course setCreditHour(double creditHour) {
         this.creditHour = creditHour;
+        return this;
+    }
+
+
+
+    public String getCourseDescription() {
+        return courseDescription;
+    }
+
+    public Course setCourseDescription(String courseDescription) {
+        this.courseDescription = courseDescription;
+        return this;
+    }
+
+    public Double getCourseMarks() {
+        return courseMarks;
+    }
+
+    public Course setCourseMarks(Double courseMarks) {
+        this.courseMarks = courseMarks;
+        return this;
+    }
+
+    public String getCourseGrade() {
+        return courseGrade;
+    }
+
+    public Course setCourseGrade(String courseGrade) {
+        this.courseGrade = courseGrade;
+        return this;
+    }
+
+    public String getCoursePrerequisite() {
+        return coursePrerequisite;
+    }
+
+    public Course setCoursePrerequisite(String coursePrerequisite) {
+        this.coursePrerequisite = coursePrerequisite;
         return this;
     }
 }
--- src/main/java/com/arafat/whiteboard/repository/CourseRepo.java
+++ src/main/java/com/arafat/whiteboard/repository/CourseRepo.java
@@ -9,4 +9,6 @@ public interface CourseRepo  extends JpaRepository<Course, Long> {
 
     Course findCourseByCourseTitleEquals(String title);
     List<Course> findAllByCreditHour(double credit_hour);
+    Course findCourseByCourseCode(String course_code);
+    List<Course> findCourseByCourseGrade(String grade);
 }
